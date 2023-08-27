0aded9ae6fdbc3cf10d08e0049cbc8a5f86dd49a
chennakesava <chennakesavulukallagunta@gmail.com>
2023-08-27 15:27:25 +0530

    VAriables concepts

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -3,3 +3,12 @@
 echo "Hello world Iam learning new things"
 echo "Iam chennakesava"
 echo "Iam btech graduate"
+echo "Today concepts we learn variables"
+
+PERSON1=suresh
+PERSON2=Ramesh
+
+echo "$PERSON1: good morning $PERSON2"
+echo "$PERSON2: VEry Good morning dear $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2:IAm good $PERSON1"
