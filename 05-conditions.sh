e01675f41f53b42321a7e51f03665d6cad5eb869
chennakesava <chennakesavulukallagunta@gmail.com>
2023-08-28 08:14:59 +0530

    conditions

---
+++ 05-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#check if the number is greater then 10 or not
+
+NUMBER=$1
+if [ $NUMBER -gt 10 ]
+then
+  echo "$NUMBER is greater then 10"
+else
+  echo "$NUMBER is not greater then 10"
+fi
